a6e49f8ba68b9b778592968e80dfe335d6839037
Josh Whetton <whetton.josh@gmail.com>
2023-12-01 10:05:27 -0500

    🧪 (test): Failing test for parsing calibration numbers
    Bare minimum failing tests.

---
+++ 1/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const (
+	inputFile = "input"
+)
+
+func parseCalibrationNumbers(input string) int {
+    return 0
+}
+
+func main() {
+	file, err := os.Open(inputFile)
+	if err != nil {
+		fmt.Printf("Error opening file: %v", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		// line = scanner.Text()
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Scanner error: %v", err)
+	}
+}
---
+++ 1/main_test.go
@@ -0,0 +1,34 @@
+package main
+
+import "testing"
+
+type ParseCalibrationTest struct {
+	Input string
+	Expected int
+	Pass bool
+}
+
+func (st ParseCalibrationTest) Test() (found int, expected bool) {
+    found = parseCalibrationNumbers(st.Input)
+    if found == st.Expected {
+        expected = true
+    } else {
+        expected = false
+    }
+    return found, expected
+}
+
+func TestParseCalibrationNumbers(t *testing.T) {
+	tests := []ParseCalibrationTest{
+        {"1abc2", 12, true},
+        {"pqr3stu8vwx", 38, true},
+        {"a1b2c3d4e5f", 15, true},
+        {"treb7uchet", 77, true},
+	}
+
+	for _, test := range tests {
+		if got, result := test.Test(); result != test.Pass {
+			t.Errorf("input: %+v. got: %d, wanted %d", test.Input, got, test.Expected)
+		}
+	}
+}
