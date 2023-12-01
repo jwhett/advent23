a86bb207ec0d19a8a8cfd82e4590df6049dc932c
Josh Whetton <whetton.josh@gmail.com>
2023-12-01 11:13:21 -0500

    ✅ (test): Correctly parsing numbers from input

--- 1/main_test.go
+++ 1/main_test.go
@@ -1,6 +1,28 @@
 package main
 
-import "testing"
+import (
+    "testing"
+    "slices"
+)
+
+type ParseNumbersTest struct {
+	Input    string
+	Expected []int
+	Pass     bool
+}
+
+func (pnt ParseNumbersTest) Test() (found []int, expected bool) {
+	found, err := parseNumbersFromString(pnt.Input)
+	if err != nil {
+    	return found, false
+	}
+	if match := slices.Compare(found, pnt.Expected); match == 0 {
+		expected = true
+	} else {
+		expected = false
+	}
+	return found, expected
+}
 
 type ParseCalibrationTest struct {
 	Input    string
@@ -8,14 +30,29 @@ type ParseCalibrationTest struct {
 	Pass     bool
 }
 
-func (st ParseCalibrationTest) Test() (found int, expected bool) {
-	found = parseCalibrationNumbers(st.Input)
-	if found == st.Expected {
+func (pct ParseCalibrationTest) Test() (found int, expected bool) {
+	found = parseCalibrationNumbers(pct.Input)
+	if found == pct.Expected {
 		expected = true
 	} else {
 		expected = false
 	}
 	return found, expected
+}
+
+func TestParseNumbersFromString(t *testing.T) {
+	tests := []ParseNumbersTest{
+		{"1abc2", []int{1, 2}, true},
+		{"pqr3stu8vwx", []int{3, 8}, true},
+		{"a1b2c3d4e5f", []int{1,2,3,4,5}, true},
+		{"treb7uchet", []int{7}, true},
+	}
+
+	for _, test := range tests {
+		if got, result := test.Test(); result != test.Pass {
+			t.Errorf("input: %+v. got: %d, wanted %d", test.Input, got, test.Expected)
+		}
+	}
 }
 
 func TestParseCalibrationNumbers(t *testing.T) {
