28e587c4bd5f9e951e8c8d99f43bb3092fbf44c4
Josh Whetton <whetton.josh@gmail.com>
2023-12-01 10:53:15 -0500

    ✅ (parseCalibration): Passing tests for parsing calibration numbers
    Yet I need to add the failing/passing tests for the helper function.

--- 1/main.go
+++ 1/main.go
@@ -4,14 +4,45 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 const (
 	inputFile = "input"
 )
 
+// parseNumbersFromString will return a list of integers found
+// in input as a slice. Error is reported when no integers are
+// found in input.
+func parseNumbersFromString(input string) ([]int, error) {
+	found := make([]int, 0)
+	for _, c := range input {
+		i, err := strconv.Atoi(string(c))
+		if err != nil {
+			continue
+		}
+		found = append(found, i)
+	}
+	if len(found) == 0 {
+		return found, fmt.Errorf("failed to parse numbers from input.")
+	}
+	return found, nil
+}
+
+// parseCalibrationNumbers will return only the "calibration number"
+// from a given line of input.
 func parseCalibrationNumbers(input string) int {
-    return 0
+	nums, err := parseNumbersFromString(input)
+	if err != nil {
+		return 0
+	}
+	// Note: Lines with a single digit will have that digit
+	// treated as the first and the last digit.
+	combined, err := strconv.Atoi(fmt.Sprintf("%d%d", nums[0], nums[len(nums)-1]))
+	if err != nil {
+		return 0
+	}
+	return combined
 }
 
 func main() {
--- 1/main_test.go
+++ 1/main_test.go
@@ -3,27 +3,27 @@ package main
 import "testing"
 
 type ParseCalibrationTest struct {
-	Input string
+	Input    string
 	Expected int
-	Pass bool
+	Pass     bool
 }
 
 func (st ParseCalibrationTest) Test() (found int, expected bool) {
-    found = parseCalibrationNumbers(st.Input)
-    if found == st.Expected {
-        expected = true
-    } else {
-        expected = false
-    }
-    return found, expected
+	found = parseCalibrationNumbers(st.Input)
+	if found == st.Expected {
+		expected = true
+	} else {
+		expected = false
+	}
+	return found, expected
 }
 
 func TestParseCalibrationNumbers(t *testing.T) {
 	tests := []ParseCalibrationTest{
-        {"1abc2", 12, true},
-        {"pqr3stu8vwx", 38, true},
-        {"a1b2c3d4e5f", 15, true},
-        {"treb7uchet", 77, true},
+		{"1abc2", 12, true},
+		{"pqr3stu8vwx", 38, true},
+		{"a1b2c3d4e5f", 15, true},
+		{"treb7uchet", 77, true},
 	}
 
 	for _, test := range tests {
