6491d5f3b4d476eb043da12e2119b56a255312bd
Josh Whetton <whetton.josh@gmail.com>
2023-12-06 10:05:07 -0500

    ♻️ (day2): Clean up the test file.
    Removed some items left over from my testing template that I didn't end up needing.

--- 2/main_test.go
+++ 2/main_test.go
@@ -5,24 +5,6 @@ package main
 
 import "testing"
 
-type GameTest struct {
-	Field    string // this will likely be a Struct to be tested.
-	Expected bool   // expected test result.
-}
-
-// Test only exists to prevent errors below.
-// These should be implemented in the non-test
-// package.
-func (st GameTest) Test() bool {
-	return true
-}
-
-var gameInput = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`
-
 func TestParseCubes(t *testing.T) {
 	cubes := parseCubes("3 blue, 4 red")
 	expected := []int{4, 0, 3}
