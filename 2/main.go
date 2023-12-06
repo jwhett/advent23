75685d1d12d13f77c19880a3b0ed5e26c5bbc1b1
Joshua Whetton <whetton.josh@gmail.com>
2023-12-05 22:18:42 -0500

    test: Failing tests for day 2 part 1
    Adding input and failing tests for some basic parsing functions for d2p1.

---
+++ 2/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+const (
+	inputFile = "input"
+)
+
+// Bag is a container from which we pull Rounds from. The
+// fields represent the maximum number of each color cube.
+type Bag struct {
+	Red, Green, Blue int
+}
+
+// Rounds are handfuls of cubes. The three fields represent
+// how many of each color were drawn from that round.
+//
+// Example: 3 blue, 4 red
+// In this example, no green cubes were pulled from the Bag.
+type Round struct {
+	Red, Green, Blue int
+}
+
+// Game represents a series of rounds. Each Game has its own
+// ID and some number of rounds that were played.
+//
+// Example: Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+// In this example, three Rounds were played in Game 1.
+type Game struct {
+	Id     int
+	Rounds []Round
+}
+
+func parseGame(line string) Game {
+	gameInfo := strings.Split(line, ": ")
+	var gameId int
+	fmt.Sscanf(gameInfo[0], "Game %d", &gameId)
+	rounds := parseRounds(gameInfo[1])
+	return Game{gameId, rounds}
+}
+
+func parseRounds(line string) []Round {
+	rounds := make([]Round, 0)
+	rawRounds := strings.Split(line, ";")
+	for _, rawRound := range rawRounds {
+		cubes := parseCubes(rawRound)
+		rounds = append(rounds, Round{cubes[0], cubes[1], cubes[2]})
+	}
+	return rounds
+}
+
+func parseCubes(input string) []int {
+	return []int{0, 0, 0}
+}
+
+var (
+	bag = Bag{
+		12,
+		13,
+		14,
+	}
+)
+
+func main() {
+	file, err := os.Open(inputFile)
+	if err != nil {
+		fmt.Printf("Error opening file: %v", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		// line = scanner.Text()
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Scanner error: %v", err)
+	}
+}
---
+++ 2/main_test.go
@@ -0,0 +1,61 @@
+// Template file only!
+// Saving some time each day to ensure I
+// actually write tests.
+package main
+
+import "testing"
+
+type GameTest struct {
+	Field    string // this will likely be a Struct to be tested.
+	Expected bool   // expected test result.
+}
+
+// Test only exists to prevent errors below.
+// These should be implemented in the non-test
+// package.
+func (st GameTest) Test() bool {
+	return true
+}
+
+var gameInput = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`
+
+func TestParseCubes(t *testing.T) {
+	cubes := parseCubes("3 blue, 4 red")
+	expected := []int{4, 0, 3}
+	for i := range cubes {
+		if cubes[i] != expected[i] {
+			t.Errorf("parseCubes: Failed to parse the correct nubmer of cubes. Expected: %v, Got: %v", expected, cubes)
+		}
+	}
+}
+
+func TestParseRound(t *testing.T) {
+	rounds := parseRounds("3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
+	if len(rounds) != 3 {
+		t.Errorf("parseRound: Failed to parse the correct number of rounds. Expected: 3, Got: %d", len(rounds))
+	}
+	round := rounds[0]
+	expectedRound := Round{4, 0, 3}
+	if round.Red != expectedRound.Red || round.Green != expectedRound.Green || round.Blue != expectedRound.Blue {
+		t.Errorf("parseRound: Failed to parse the number of cubes in round. Expected: %v, Got: %v", expectedRound, round)
+	}
+}
+
+func TestParseGame(t *testing.T) {
+	game := parseGame("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
+	expected := Game{
+		1,
+		[]Round{
+			Round{4, 0, 3},
+			Round{1, 2, 6},
+			Round{0, 2, 0},
+		},
+	}
+	if len(game.Rounds) != len(expected.Rounds) {
+		t.Errorf("parseGame: Unexpected number of rounds parsed. Expected: %d, Got: %d", len(expected.Rounds), len(game.Rounds))
+	}
+}
